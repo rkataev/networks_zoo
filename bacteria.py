d465b497a213d5262806125203976154a8be9611
neuro <nanenaro@gmail.com>
2018-06-22 14:26:56 +0300

    Initial commit

---
+++ bacteria.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*
+from __future__ import print_function
+
+import tensorflow as tf
+print("версия тезорфлоу: " + str(tf.__version__))
+import keras
+print("версия керас: " + str(keras.__version__))
+from keras import backend as K
+if K.image_data_format() == 'channels_first':
+    print("количество каналов - первое в кортеже")
+else:
+    print("количество каналов - последнее в кортеже")
+
+print ("image dim ordering: " + K.image_dim_ordering() )
+import numpy as np
+from time import time
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv1D, MaxPooling1D
+from keras.callbacks import TensorBoard
+from dataset_getter import open_dataset
+
+batch_size = 50
+num_classes = 5
+epochs = 10
+
+# солько тактов времени в кардиограме
+img_len = 5000
+
+# олучаем данные, сразу трейн и тест
+x_train, x_test, y_train, y_test = open_dataset()
+
+
+# простейшая сверточная моделька
+model = Sequential()
+model.add(Conv1D(20,
+                 kernel_size=5,
+                 activation='relu',
+                 input_shape=(img_len, 1)))
+
+model.add(MaxPooling1D(pool_size=2))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.summary()
+
+# коллбеки
+callbacks = []
+boardwriter = TensorBoard(log_dir='./logs_bacteria',
+                          histogram_freq=1,
+                          write_graph=True,
+                          write_grads=True,
+                          write_images=True)
+callbacks.append(boardwriter)
+
+# само обучение
+model.fit(x=x_train, y=y_train, verbose=1,validation_data=(x_test, y_test), epochs=epochs, callbacks=callbacks)
---
+++ dataset_getter.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*
+#from sklearn.model_selection import train_test_split, learning_curve, KFold
+from sklearn.model_selection import train_test_split
+from pprint import pprint
+
+import numpy as np
+import easygui
+import pickle as pkl
+def open_dataset():
+    filename_pkl = easygui.fileopenbox("выберите файл с датасетом")
+
+    print("загружаем 2д-датасет из файла " + filename_pkl)
+    infile = open(filename_pkl, 'rb')
+    new_dict = pkl.load(infile)
+    infile.close()
+
+    assert len(new_dict['x']) == len(new_dict['y'])
+    x, y = np.array(new_dict['x']), np.array(new_dict['y'])
+    print(len(new_dict['x']) ," записей ")
+
+    # разбиваем на трейн/тест
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
+    # решейпим их, чтоб модель могла их сожрать
+    x_train = np.expand_dims(x_train, axis=2)  # reshape (n1, n2) -> (n1, n2, 1)
+    x_test = np.expand_dims(x_test, axis=2)  # reshape (n1, n2) -> (n1, n2, 1)
+
+
+    print("y_train  имеет форму " + str(y_train.shape))
+    print("x_train  имеет форму " + str(x_train.shape))
+    print("x_test   имеет форму " + str(x_test.shape))
+    print("y_test   имеет форму " + str(y_test.shape))
+
+
+    return  x_train, x_test, y_train, y_test
+
+if __name__ == "__main__":
+    x_train, x_test, y_train, y_test = open_dataset()
