4024db7d6176e6ea6057cee62f2e4c7e9a4ba5df
neuro <nanenaro@gmail.com>
2018-06-26 18:43:46 +0300

    амба

--- ameba_family/ameba.py
+++ ameba_family/ameba.py
@@ -4,16 +4,18 @@
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Input
+from keras.layers import Dense, Dropout, Flatten, Input, Add
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
 from keras import backend as K
 from keras import Model
 from keras.utils import np_utils
-from keras.callbacks import TensorBoard
+from keras.callbacks import TensorBoard, ModelCheckpoint
 from sklearn.metrics import label_ranking_average_precision_score
+from keras.models import model_from_json
 
 import matplotlib.pyplot as plt
 import numpy as np
+from keras.utils import plot_model
 
 from ameba_family import amoeba_proteus
 
@@ -33,37 +35,82 @@ y_test = np_utils.to_categorical(y_test)
 
 
 def train():
-    amoeba = amoeba_proteus.AmoebaProteus(len_noise=9, side_corrupter=4)
+    amoeba = amoeba_proteus.AmoebaProteus(side_corrupter=4)
     ae = amoeba.get_ae()
     ae.summary()
     ae.compile(optimizer='adadelta', loss='binary_crossentropy')
-    noise_input_data_train = amoeba.get_noise_tensor(len=len(X_train))
-    noise_input_data_test = amoeba.get_noise_tensor(len=len(X_test))
+
     MAP_OF_INPUT_TENSORS = {'code_input': y_train,
-                            'raw_image_input': X_train,
-                            'noise_input': noise_input_data_train}
+                            'raw_image_input': X_train
+                            }
     MAP_OF_INPUT_TENSORS_TEST = {'code_input': y_test,
-                            'raw_image_input': X_test,
-                            'noise_input': noise_input_data_test}
+                            'raw_image_input': X_test
+                            }
     X_train_flatten = X_train.reshape(len(X_train), 784)
     X_test_flatten = X_test.reshape(len(X_test), 784)
 
     callbacks = []
-    boardwriter = TensorBoard(log_dir='./logs_ameba',
+    boardwriter = TensorBoard(log_dir='./logs_amoeba',
                               histogram_freq=1,
                               write_graph=True,
                               write_grads=True,
                               write_images=True)
     callbacks.append(boardwriter)
+    filepath = "amoeba_proteus_best.hdf5"
+    best_weights_savier = ModelCheckpoint(save_best_only=True, save_weights_only=True, filepath=filepath, monitor='val_acc', verbose=1,  mode='max')
+    callbacks.append(best_weights_savier)
 
     ae.fit(MAP_OF_INPUT_TENSORS,
               {'decoded': X_train_flatten},
                 validation_data=(MAP_OF_INPUT_TENSORS_TEST, {'decoded': X_test_flatten}),
-              epochs=50, batch_size=32, callbacks=callbacks)
+              epochs=1, batch_size=40, callbacks=callbacks)
+    # охранение модели
+    model_json = ae.to_json()
+    with open("amoeba_proteus.json", "w") as json_file:
+        json_file.write(model_json)
+    ae.save_weights("amoeba_proteus.hdf5", overwrite=True)
+
+def load(filename_weights="amoeba_proteus.hdf5", model_json='amoeba_proteus.json'):
+    json_file = open(model_json, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_amoeba = model_from_json(loaded_model_json)
+    print("построили пустую амебу")
+    print("загружаем веса в амебу с диска")
+    loaded_amoeba.load_weights(filename_weights)
+    loaded_amoeba.compile(optimizer='adadelta', loss='binary_crossentropy')
+    return loaded_amoeba
+
+def get_conditioned_predictions(amoeba):
+    ys= np.array(range(0,9,1))
+    ys = np_utils.to_categorical(ys)
+
+
+    x = X_test[0]
+    xs = []
+    for i in range(9):
+        xs.append(x)
+    xs = np.array(xs)
+
+    print ( ys.shape," ys shape")
+    print ( xs.shape, " xs shape")
+
+    #теперь генерим репрезентации
+
+    MAP_OF_INPUT_TENSORS = {'code_input': y_train,
+                            'raw_image_input': X_train}
+    predictions = amoeba.predict(MAP_OF_INPUT_TENSORS)
+    return predictions
+
+
 
 
 if __name__ == "__main__":
-    train()
+    #train()
+    trained_amoeba = load()
+    predictions = get_conditioned_predictions(trained_amoeba)
 
 
-# затем из тестового куска данных берем картинку и сравниваем ее восстановление с доп. входом и без.
+
+
+
--- ameba_family/amoeba_proteus.py
+++ ameba_family/amoeba_proteus.py
@@ -1,7 +1,7 @@
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Input
+from keras.layers import Dense, Dropout, Flatten, Input, Concatenate
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
 from keras import backend as K
 from keras import Model
@@ -15,29 +15,29 @@ import numpy as np
 from ameba_family import _corrupters as corrupt
 
 class AmoebaProteus:
-    def __init__(self, len_noise, side_corrupter):
-        self.len_of_noise = len_noise
+    def __init__(self, side_corrupter):
         self.side_corrupter = side_corrupter
 
 
     def get_ae(self):
         code_input = Input(shape=(10,), name='code_input')
-        code_embedding = Dense(20)(code_input)
+        code_embedding = Dense(20, name="code_embedding")(code_input)
 
-        noise_input = Input(shape=(self.len_of_noise,), name='noise_input')
+
 
         non_corrupted_images, corrupted = corrupt.get_corrupted(self.side_corrupter)
 
-        corrupted_input_embedding = Dense(20)(corrupted)
-        corrupted_input_embedding = Flatten()(corrupted_input_embedding)
+        corrupted_input_embedding = Dense(20, name="corrupted_input_embedding")(corrupted)
+        corrupted_input_embedding = Flatten(name = "faltten_embedding")(corrupted_input_embedding)
 
-        x = keras.layers.concatenate([corrupted_input_embedding, code_embedding, noise_input])
+        x = keras.layers.concatenate([corrupted_input_embedding, code_embedding])
 
         decoded = Dense(100, activation='relu')(x)
         decoded = Dropout(rate=0.25)(decoded)
         decoded = Dense(784, activation='sigmoid', name='decoded')(decoded)
-        ae_model = Model(inputs=[non_corrupted_images, code_input, noise_input], outputs=decoded)
+        ae_model = Model(inputs=[non_corrupted_images, code_input], outputs=decoded)
         return ae_model
 
-    def get_noise_tensor(self, len):
-        return np.random.normal(size=(len, self.len_of_noise,))
+
+
+
