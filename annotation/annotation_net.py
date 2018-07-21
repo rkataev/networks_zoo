70400d17d6b953144d91f21d779ce45abe53ee1a
namenaro <nanenaro@gmail.com>
2018-07-21 19:40:58 +0300

    черновичок визуализатора аннотатора

--- annotation/annotation_net.py
+++ annotation/annotation_net.py
@@ -74,7 +74,7 @@ def eval_models_in_folder():
 if __name__ == "__main__":
     name = "sofia_annotator"
     #train(name)
-
+    #eval_models_in_folder()
     from tensorflow.python.client import device_lib
 
     print(device_lib.list_local_devices())
